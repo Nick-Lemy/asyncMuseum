ae9b33d2a12750fc9819ffcc100b654f07284929
Nick-Lemy <n.kayiranga@alustudent.com>
2025-03-23 15:49:54 +0200

    update to full backend project

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,23 @@
 import express from 'express'
 const app = express()
+
+// Middlewares
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
 app.get('/', (req, res)=>{
-    return res.send('Hey there!')
+    res.render('')
 })
+
+app.get("/exhibits", (req, res) => {
+    const exhibits = [
+      { id: 1, name: "Mona Lisa", description: "Leonardo da Vinci masterpiece" },
+      { id: 2, name: "Starry Night", description: "Van Gogh painting" },
+    ];
+  
+    res.render("index", { exhibits });
+  });
 app.listen(3612, ()=>{
     console.log('http://localhost:3612')
 })
